27e64ff17c520bbca492a06f3a25c7bcf3342f3a
JohanK <klausenjohan23@gmail.com>
2021-09-20 10:00:12 +0200

    gör koden med buttonhandler

--- Calc2.0/Form1.cs
+++ Calc2.0/Form1.cs
@@ -15,6 +15,20 @@ namespace Calc2._0
         public Form1()
         {
             InitializeComponent();
+            button1.Text = "+";
+            button2.Text = "-";
+            button3.Text = "/";
+            button4.Text = "*";
+
+            button1.Click += new EventHandler(ButtonHandler);
+            button2.Click += new EventHandler(ButtonHandler);
+            button3.Click += new EventHandler(ButtonHandler);
+            button4.Click += new EventHandler(ButtonHandler);
+        }
+
+        private void ButtonHandler(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
